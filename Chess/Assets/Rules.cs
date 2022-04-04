4c9a41e1caa83b492e6e05834f9602e91f0abe71
kritikall177 <87034338+kritikall177@users.noreply.github.com>
2022-04-04 12:53:55 +0300

    listing 0.1

---
+++ Chess/Assets/Rules.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rules : MonoBehaviour
+{
+    DragAndDrop dad;
+
+    public Rules()
+    {
+        dad = new DragAndDrop();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dad.Action(); 
+    }
+}
+
+class DragAndDrop
+{
+    enum State
+    {
+        none,
+        drag
+    }
+
+    State state;
+    GameObject item;
+
+    public DragAndDrop()
+    {
+        state = State.none;
+        item = null;
+    }
+
+    public bool Action()
+    {
+        switch (state)
+        {
+            case State.none: 
+                if(IsMouseButtonPressed()) PickUp();
+                break;
+            case State.drag:
+                if (IsMouseButtonPressed()) Drag();
+                else
+                {
+                    Drop();
+                    return true;
+                }
+
+                break;
+        }
+        return false;
+    }
+
+    bool IsMouseButtonPressed()
+    {
+        return Input.GetMouseButton(0);
+    }
+
+    void PickUp()
+    {
+        Vector2 clickPosition = GetClickPosition();
+        Transform clickedItem = GetItemAt(clickPosition);
+        if(clickedItem == null) return;
+        item = clickedItem.gameObject;
+        state = State.drag;
+        Debug.Log("Picked up" + item.name);
+    }
+
+    Vector2 GetClickPosition()
+    {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    Transform GetItemAt (Vector2 position)
+    {
+        RaycastHit2D[] figure = Physics2D.RaycastAll(position, position, 0.5f);
+        if (figure.Length == 0) return null;
+        return figure[0].transform;
+    }
+
+    void Drag()
+    {
+        item.transform.position = GetClickPosition();
+    }
+    void Drop()
+    {
+        state = State.none;
+        item = null;
+    }
+}
